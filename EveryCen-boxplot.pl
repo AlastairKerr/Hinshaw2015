187b41f91ba35c33a3bbc534127286b78c279c6d
Alastair Kerr <alastair.kerr@ed.ac.uk>
2015-05-06 15:15:32 +0100

    Scripts

--- EveryCen-boxplot.pl
+++ EveryCen-boxplot.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-open(A, "../../CENTROMERE.gff") or die "CENTROMERE.gff is required";
+open(A, "./CENTROMERE.gff") or die "CENTROMERE.gff is required";
 
 my %type;
 my %start;
