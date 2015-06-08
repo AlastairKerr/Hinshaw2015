74a9fc5a82d0226022a0c81f2162e68ccc1b4c96
Alastair Kerr <alastair.kerr@ed.ac.uk>
2015-06-08 13:36:48 +0100

    median script bugfix

--- plot_median.r
+++ plot_median.r
@@ -1,14 +1,15 @@
 #Load data 
-# data has been processed,
-# ChIP over input, and 1 row per chromosome, 1 column = 100bp  window
-# No header or row coordinates 
-
 am15540 <- read.table("AM15540_Scc1-6HA_2hrNocodazole_SCC4-HIS3-rDNA-all.50k")
 am15537 <- read.table("AM15537_Scc1-6HA_2hrNocodazole_scc4-m35-rDNA-all.50k")
 
-#calculate the median per window
-am40_median <- t(as.data.frame(am15540) %>% summarise_each(funs(median(., na.rm=TRUE) )) )
-am37_median <- t(as.data.frame(am15537) %>% summarise_each(funs(median(., na.rm=TRUE) )) )
+library(dplyr)
+
+#calculate the median per region 
+am40_median <- as.data.frame(am15540) %>% summarise_each(funs(median(., na.rm=TRUE) )) 
+am37_median <- as.data.frame(am15537) %>% summarise_each(funs(median(., na.rm=TRUE) )) 
+
+#set the coordinates 
+Coords <- (1:499 * 100) -25000
 
 #Plot used in figure 4 
 svg("cenmedian_15537v15540.svg", width=15)
